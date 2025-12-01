78b8713b5b6e89750e75974d304a122044eb6b0a
shiina04jbs10 <shiina04jbs10@gmail.com>
2025-12-01 22:17:03 +0900

    a

--- Assets/Script/player_move.cs
+++ Assets/Script/player_move.cs
@@ -1,3 +1,4 @@
+using Unity.Cinemachine;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -5,6 +6,8 @@ public class player_move : MonoBehaviour
 {
     [SerializeField] GameObject Body;
     [SerializeField] float Speed;
+    [SerializeField] float Roteto;
+    [SerializeField] CinemachineCamera Camera;
     Rigidbody rb;
 
     //--------
@@ -22,5 +25,16 @@ public class player_move : MonoBehaviour
     private void Update()
     {
         rb.linearVelocity = new Vector3(Inputvec.x * Speed, rb.linearVelocity.y, Inputvec.y * Speed);
+
+
+        //------  ------
+        if (Inputvec.magnitude < 0.1f){
+            return;
+        }
+
+
+        var angle = Mathf.Atan2(Inputvec.x, Inputvec.y) * Mathf.Rad2Deg;
+        var q = Quaternion.Euler(0, angle, 0);
+        Body.transform.localRotation = Quaternion.RotateTowards(Body.transform.localRotation, q, Roteto);
     }
 }
